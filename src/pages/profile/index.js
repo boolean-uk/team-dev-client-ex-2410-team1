f5263a44bdecd9b3757d57ac140593a755e780e3
Dag Andreas <daanfofo@gmail.com>
2024-11-01 08:46:45 +0100

    re-merged with main

--- src/pages/profile/index.js
+++ src/pages/profile/index.js
@@ -1,7 +1,7 @@
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import Avatar from '../../components/avatar';
-import { getUserById, patchUserById } from '../../service/apiClient';
+import { getUserById } from '../../service/apiClient';
 import useAuth from '../../hooks/useAuth';
 import Form from '../../components/form';
 import TextInput from '../../components/form/textInput';
